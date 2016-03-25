d13b0130295bc9a360ac0144aa81b0a1867f0e80
flaneuse <laura.d.hughes@gmail.com>
2016-03-25 08:58:36 -0400

    update import

--- WAFR_importDHS.R
+++ WAFR_importDHS.R
@@ -29,31 +29,37 @@
 # Median age at first sexual intercourse: 25-49 (i36848B0)
 # Women who are literate (i797B5DA)
 
-# require(RJSONIO)
+
+library(llamar)
+loadPkgs()
 
 
+# set up the queries ------------------------------------------------------
 
-
-# set up the queries
 apiKey = 'USAAID-405632'
-indicator1 = 'FP_NADM_W_UNT,FE_FRTR_W_TFR,CN_BFDR_C_MDE,HA_CPHT_W_T1R,HC_ELEC_H_ELC'
-indicator2 = 'CN_NUTS_C_WA2,HA_HIVP_W_HIV,HA_HIVP_W_HVE'
-indicator3 = 'HA_HIVP_B_HIV,HA_HIVP_B_HVE,CM_ECMT_C_U5M,CM_ECMR_C_IMR'
-indicator4 = 'CM_ECMR_C_NNR,CH_VACS_C_BAS,CH_VACS_C_NON,CH_VACC_C_BAS,CH_VACC_C_NON'
-indicator5 = 'CH_VAC1_C_BAS,CH_VAC1_C_NON,CH_DIAT_C_ORT,CH_DIAR_C_DIA,MA_AAFM_W_M20'
-indicator6 = 'MA_AAFM_W,MA_AAFM_W,MA_AAFM_W,MA_AAFM_W,MA_AAFM_W,MA_AAFM_W,MA_AAFM_W_M2'
-indicator7 = 'HA_HIVP_M_HVE,HA_HIVP_M_HIV'
-indicator8 = 'CN_NUTS_C_WH2,CN_NUTS_C_HA2,ED_EDUC_W_SEP'
-indicator9 = 'CM_ECMR_C_PNR,FP_CUSA_W_MOD,FP_CUSM_W_ANY,FP_CUSM_W_MOD'
-indicator10 = 'SX_AAFS_W_M25,SX_AAFS_W_M30,SX_AAFS_W_M35,SX_AAFS_W_M40,SX_AAFS_W_M45'
-indicator11 = 'SX_AAFS_W_M2A,SX_AAFS_W_M2B,ED_LITR_W_LIT,RH_PAHC_W_KNW,RH_PAHC_W_PRM'
-indicator12 = 'RH_PAHC_W_MON,RH_PAHC_W_DIS,RH_PAHC_W_TRN,RH_PAHC_W_ALN,RH_PAHC_W_FEM'
-indicator13 = 'MM_MMRT_W_MRT,MM_MMRO_W_GFR,MM_MMRO_W_MMR,MM_MMRO_W_CIL,MM_MMRO_W_CIH'
-indicator14 = 'RH_DELP_C_DHF,FP_CUSA_W_ANY,SX_AAFS_W_M20'
+indicator = NULL
+indicator[1] = 'FP_NADM_W_UNT,FE_FRTR_W_TFR,CN_BFDR_C_MDE,HA_CPHT_W_T1R,HC_ELEC_H_ELC'
+indicator[2] = 'CN_NUTS_C_WA2,HA_HIVP_W_HIV,HA_HIVP_W_HVE'
+indicator[3] = 'HA_HIVP_B_HIV,HA_HIVP_B_HVE,CM_ECMT_C_U5M,CM_ECMR_C_IMR'
+indicator[4] = 'CM_ECMR_C_NNR,CH_VACS_C_BAS,CH_VACS_C_NON,CH_VACC_C_BAS,CH_VACC_C_NON'
+indicator[5] = 'CH_VAC1_C_BAS,CH_VAC1_C_NON,CH_DIAT_C_ORT,CH_DIAR_C_DIA,MA_AAFM_W_M20'
+indicator[6] = 'MA_AAFM_W,MA_AAFM_W,MA_AAFM_W,MA_AAFM_W,MA_AAFM_W,MA_AAFM_W,MA_AAFM_W_M2'
+indicator[7] = 'HA_HIVP_M_HVE,HA_HIVP_M_HIV'
+indicator[8] = 'CN_NUTS_C_WH2,CN_NUTS_C_HA2,ED_EDUC_W_SEP'
+indicator[9] = 'CM_ECMR_C_PNR,FP_CUSA_W_MOD,FP_CUSM_W_ANY,FP_CUSM_W_MOD'
+indicator[10] = 'SX_AAFS_W_M25,SX_AAFS_W_M30,SX_AAFS_W_M35,SX_AAFS_W_M40,SX_AAFS_W_M45'
+indicator[11] = 'SX_AAFS_W_M2A,SX_AAFS_W_M2B,ED_LITR_W_LIT,RH_PAHC_W_KNW,RH_PAHC_W_PRM'
+indicator[12] = 'RH_PAHC_W_MON,RH_PAHC_W_DIS,RH_PAHC_W_TRN,RH_PAHC_W_ALN,RH_PAHC_W_FEM'
+indicator[13] = 'MM_MMRT_W_MRT,MM_MMRO_W_GFR,MM_MMRO_W_MMR,MM_MMRO_W_CIL,MM_MMRO_W_CIH'
+indicator[14] = 'RH_DELP_C_DHF,FP_CUSA_W_ANY,SX_AAFS_W_M20'
 
-indicators = paste0(indicator1, indicator2, indicator3, indicator4, indicator5, indicator6, indicator7, 
-                    indicator8, indicator9, indicator10, indicator11, indicator12, indicator13, indicator14)
+indicators = paste0(indicator[1], ',', indicator[2], ',', indicator[3], ',', indicator[4], ',', indicator[5],
+                    ',', indicator[6], ',', indicator[7], ',', 
+                    indicator[8], ',', indicator[9], ',', indicator[10], ',', 
+                    indicator[11], ',', indicator[12], ',', indicator[13], ',', indicator[14])
+
 countries = 'BJ,BF,CM,CI,GH,GN,LB,ML,MR,NI,NG,SN,SL,TG'
+
 # years = paste0(seq(1980,2016), collapse = ',')
 years = paste0(seq(2000,2016), collapse = ',')
 
@@ -70,9 +76,7 @@ json_data <- lapply(json_file$Data, function(x) { unlist(x) })
 # Convert JSON input to a data frame
 allData <- as.data.frame(do.call("rbind", json_data),stringsAsFactors=FALSE)
 
-library('dplyr')
-library('ggplot2')
-library('llamar')
+
 
 svyYrs = allData %>% 
   group_by(SurveyYear, CountryName) %>% 
@@ -87,32 +91,21 @@ ggplot(svyYrs, aes(x = year, y = CountryName, size = num)) +
 ggplot(svyYrs, aes(x = year, y = CountryName, size = num)) +
   geom_point() +
   scale_x_continuous(limits = c(2000, 2016),
-    breaks = seq(2000, 2016, by = 2)) +
+                     breaks = seq(2000, 2016, by = 2)) +
   theme_xgrid()
 
 
 years = paste0(seq(2000,2016), collapse = ',')
 
 # Import DHS Indicator National-level data
-json_file = fromJSON(paste0('http://api.dhsprogram.com/rest/dhs/data?breakdown=national&indicatorIds=',indicators, 
-                             '&countryIds=', countries, 
-                             '&SurveyYear=', years,
-                             '&apiKey=', apiKey,
-                             '&perpage=5000'))
-                             
-json_file_sub1 = fromJSON(paste0('http://api.dhsprogram.com/rest/dhs/data?breakdown=all&indicatorIds=',indicator2, 
-                             '&countryIds=', countries, 
-'&SurveyYear=', years,
-'&apiKey=', apiKey,
-'&perpage=5000'))
+
+natl = loadDHS(breakdown = 'national', indicators = indicators, 
+               countries = countries, years = years, apiKey = apiKey, numResults = 5000)
+
+
 
 x = loadDHS(breakdown = 'national', indicators = indicator14, countries = countries, years = years, apiKey = apiKey, numResults = 100)
 
-# Unlist the JSON file entries
-json_data <- lapply(json_file$Data, function(x) { unlist(x) })
-
-# Convert JSON input to a data frame
-natl <- as.data.frame(do.call("rbind", json_data),stringsAsFactors=FALSE)
 
 
 write.csv(natl, '~/Documents/USAID/West Africa Regional 2016/dataout/WAFR_DHS_natl.csv')
