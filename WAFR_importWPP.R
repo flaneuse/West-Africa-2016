71c3f4c26381f305ddef37969d0debe4204dadff
flaneuse <laura.d.hughes@gmail.com>
2016-03-30 11:16:54 -0400

    pop plots

--- WAFR_importWPP.R
+++ WAFR_importWPP.R
@@ -27,6 +27,7 @@ predPop = read_excel('~/Documents/USAID/West Africa Regional 2016/dataout/WAFR_U
 obsPop = obsPop %>% 
   gather(country, pop, -year) %>% 
   mutate(pop = pop/1000, # convert to millions
+         year = as.numeric(year), # convert to number
          isCountry = ifelse(country %in% c('Sub-Saharan Africa', 'Africa', 'Western Africa'), 
                             0, 1),
          proj = 0
@@ -36,10 +37,55 @@ predPop = predPop %>%
   gather(country, pop, -year) %>% 
   filter(year != 2015) %>% 
   mutate(pop = pop/1000, # convert to millions
+         year = as.numeric(year), # convert to number
          isCountry = ifelse(country %in% c('Sub-Saharan Africa', 'Africa', 'Western Africa'), 
                             0, 1),
          proj = 1
   ) 
 
-pop = rbind(obsPop, predPop)
+pop = rbind(obsPop, predPop) 
 
+
+# calc derivative ---------------------------------------------------------
+popRate = pop %>% 
+  arrange(country, year) %>% 
+  mutate(lagged = lag(pop),
+         lagged = ifelse(year == 1950, NA, lagged),
+         rate = (pop - lagged)/lagged)
+
+
+# plots -------------------------------------------------------------------
+
+
+accentColor = '#b2182b'
+
+obsPop2 = obsPop %>% filter(isCountry == 1)
+
+ggplot(obsPop2, aes(x = year, y = pop, 
+                   label = paste0(round(pop, 1), ' M'),
+                group = country)) +
+  geom_line(colour = accentColor) +
+  geom_line(colour = accentColor, linetype = 2,
+            data = predPop) +
+  geom_point(size = 2, colour = accentColor,
+             fill = 'white', shape = 21,
+             data = obsPop %>% filter(year == 2015)) +
+  geom_text(size = 3, colour = accentColor,
+            nudge_y = 1,
+             data = obsPop %>% filter(year == 2015)) +
+  facet_wrap(~country, scales = 'free_y') 
+
+
+ggplot(popRate, aes(x = year, y = rate, 
+                    label = percent(rate, 1),
+                    group = country)) +
+  geom_hline(colour = grey90K, yintercept = 0) +
+  geom_line(colour = accentColor, linetype = 2) +
+  geom_line(colour = accentColor, data = popRate %>% filter(proj == 0)) +
+  geom_point(size = 2, colour = accentColor,
+             fill = 'white', shape = 21,
+             data = popRate %>% filter(year == 2015)) +
+  geom_text(size = 3, colour = accentColor,
+            nudge_y = 0.1,
+            data = popRate %>% filter(year == 2015)) +
+  facet_wrap(~country)
