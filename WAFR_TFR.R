e626df9a901ea2156052e5ec6c696571d613a4e0
flaneuse <laura.d.hughes@gmail.com>
2016-04-06 12:57:07 -0400

    tfr plots

--- WAFR_TFR.R
+++ WAFR_TFR.R
@@ -4,7 +4,7 @@ loadPkgs()
 tfr = read_excel('~/Documents/USAID/West Africa Regional 2016/datain/WPP_TFR.xlsx',
                     sheet = 1)
 
-prb = read.csv('~/Documents/USAID/West Africa Regional 2016/datain/WAFR_PRB_TFR.csv') %>% 
+prb = read_excel('~/Documents/USAID/West Africa Regional 2016/datain/WAFR_PRB_TFR.xlsx') %>% 
   mutate(prb = as.numeric(Data)
          # country = rem
          # country = ifelse(country == 'Cape Verde', 'Cabo Verde', country)
@@ -16,23 +16,34 @@ accentColor = '#b2182b'
 # Pull out the regional numbers
 regionalTFR = tfr %>% 
   select(year,
-         tfrWAfr = `Western Africa`)
+         tfrWAfr = `Western Africa`) %>% 
+  mutate(laggedWAfr = lag(tfrWAfr),
+         rateWAfr = (tfrWAfr - laggedWAfr)/laggedWAfr)
 
 tfr = tfr %>% 
   gather(country, tfr, -year) %>% 
-  filter(!(country %in% c('Sub-Saharan Africa', 'AFRICA', 'Western Africa')))
+  filter(!(country %in% c('Sub-Saharan Africa', 'AFRICA', 'Western Africa'))) %>% 
+  group_by(country) %>% 
+  mutate(lagged = lag(tfr),
+         rate = (tfr - lagged)/lagged
+         )
 
-tfr = full_join(tfr, regionalTFR)
+tfr = full_join(tfr, regionalTFR) %>% 
+  mutate(diffRate = rate-rateWAfr)
 
 tfr = left_join(tfr, prb %>% filter(TimeFrame == 2014), by = c("country" = "Location"))
 
 
 order = tfr %>% 
+  ungroup() %>% 
   filter(year =='2010-2015') %>% 
   arrange(desc(tfr))
 
 tfr$country = factor(tfr$country,
                      levels  = order$country)
+
+
+# TFR over time -----------------------------------------------------------
 
 ggplot(tfr, aes(x = year, y = tfr, group = country)) +
   geom_line(colour = grey50K,
@@ -54,11 +65,38 @@ ggplot(tfr, aes(x = year, y = tfr, group = country)) +
             hjust = 1,
             data = tfr %>% filter(year == '2010-2015')) +
   facet_wrap(~country) +
-  scale_x_discrete(labels = c('1950-1955','', '', '', '',
-                              '', '1980-1985', '', '','',
+  scale_x_discrete(breaks = c('1950-1955','1960-1965', '1970-1975', 
+                              '1980-1985', '1990-1995', '2000-2005','2010-2015'),
+                   labels = c('1950-1955','',  '',
+                              '1980-1985', 
                               '','','2010-2015')) +
   theme_xygrid() + 
   scale_y_continuous(limits = c(0,8)) +
   ggtitle('Niger and Mali have high Total Fertility Rates relative to the rest of the region') +
   theme(axis.text.x = element_text(size = 10),
         panel.margin = unit(1, 'lines'))
+
+
+# TFR rate over time ------------------------------------------------------
+
+ggplot(tfr, aes(x = year, y = rate, group = country)) +
+  geom_line(colour = grey50K,
+            aes(y = rateWAfr)) +
+  geom_line(colour = accentColor) +
+  geom_ribbon(aes(ymin = rateWAfr, ymax = rate),
+              fill = 'blue',
+              alpha = 0.15) +
+  geom_point(colour = accentColor,
+             data = tfr %>% filter(year == '2010-2015'))+
+  facet_wrap(~country) +
+  scale_x_discrete(breaks = c('1950-1955','1960-1965', '1970-1975', 
+                              '1980-1985', '1990-1995', '2000-2005','2010-2015'),
+                   labels = c('1950-1955','',  '',
+                              '1980-1985', 
+                              '','','2010-2015')) +
+  theme_xygrid() + 
+  scale_y_continuous(labels = scales::percent) +
+  ggtitle('Niger and Mali have high Total Fertility Rates relative to the rest of the region') +
+  theme(axis.text.x = element_text(size = 10),
+        panel.margin = unit(1, 'lines'))
+
