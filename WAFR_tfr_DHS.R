a626af3b8f46d99120e41f0c1b0502cfa2fa8b88
flaneuse <laura.d.hughes@gmail.com>
2016-04-07 00:04:33 -0400

    updates

--- WAFR_tfr_DHS.R
+++ WAFR_tfr_DHS.R
@@ -1,3 +1,6 @@
+library(llamar)
+loadPkgs()
+
 tfr = read.csv('data/WFAR_tfr.csv') %>% 
   filter(year == '2010-2015') %>% 
   select(country, tfr) %>% 
@@ -7,7 +10,6 @@ tfr = read.csv('data/WFAR_tfr.csv') %>%
   
 natl = read.csv('~/Documents/USAID/West Africa Regional 2016/dataout/WAFR_DHS_natl.csv')
 
-natl = left_join(natl, tfr, by = c('CountryName' = 'region'))
 
 
 df = natl %>% 
@@ -16,7 +18,10 @@ df = natl %>%
          SurveyYear > 2010,
          ByVariableLabel == 'Total' | ByVariableLabel == "") %>% 
   select(CountryName, IndicatorId, Value, SurveyYear) %>% 
-  spread(IndicatorId, Value)
+  spread(IndicatorId, Value) %>% 
+  filter(SurveyYear != 2012 | CountryName != 'Senegal')
+
+df = left_join(df, tfr, by = c('CountryName' = 'region'))
 
 
 # desires -----------------------------------------------------------------
@@ -26,7 +31,8 @@ ggplot(df, aes(x = FP_CUSA_W_MOD, y = 100- PR_DESL_W_WNM,
                colour = FP_NADM_W_UNT,
                size = FP_NADM_W_UNT,
                label = CountryName)) +
-  geom_label(nudge_y = 2, size = 5) + 
+  geom_label(nudge_y = 2, size = 5,
+             family = 'Segoe UI Semilight') + 
   geom_point() +
   xlab('modern contraception use in all women') +
   ggtitle('')+
@@ -36,19 +42,29 @@ ggplot(df, aes(x = FP_CUSA_W_MOD, y = 100- PR_DESL_W_WNM,
                          name = 'unmet need',
                          limits = c(7, 35)) +
   theme_xygrid() +
-  guides(fill = guide_colorbar(ticks = FALSE))+ 
+  guides(colour = guide_colorbar(ticks = FALSE))+ 
   theme(legend.position = c(0.1, 0.1))
+
+ggsave("~/GitHub/babies_contraception_unmet.pdf",
+       width = 6, height = 4,
+       bg = 'transparent',
+       paper = 'special',
+       units = 'in',
+       useDingbats=FALSE,
+       compress = FALSE,
+       dpi = 300)
 
 
 
 # tfr ---------------------------------------------------------------------
 
 
-ggplot(df, aes(x = FP_CUSA_W_MOD, y = 100- PR_DESL_W_WNM,
+ggplot(df, aes(x = FP_CUSA_W_MOD, y = tfr,
                colour = FP_NADM_W_UNT,
                size = FP_NADM_W_UNT,
                label = CountryName)) +
-  geom_label(nudge_y = 2, size = 5) + 
+  geom_label(nudge_y = 0.3, size = 5,
+             family = 'Segoe UI Semilight') + 
   geom_point() +
   xlab('modern contraception use in all women') +
   ggtitle('')+
@@ -58,5 +74,75 @@ ggplot(df, aes(x = FP_CUSA_W_MOD, y = 100- PR_DESL_W_WNM,
                          name = 'unmet need',
                          limits = c(7, 35)) +
   theme_xygrid() +
-  guides(fill = guide_colorbar(ticks = FALSE))+ 
-  theme(legend.position = c(0.1, 0.1))
+  guides(colour = guide_colorbar(ticks = FALSE))+ 
+  theme(legend.position = c(0.9, 0.9))
+
+ggsave("~/GitHub/tfr_contraception_unmet.pdf",
+       width = 6, height = 4,
+       bg = 'transparent',
+       paper = 'special',
+       units = 'in',
+       useDingbats=FALSE,
+       compress = FALSE,
+       dpi = 300)
+
+
+# TFR babies --------------------------------------------------------------
+
+ggplot(df, aes(x = 100- PR_DESL_W_WNM, y = tfr,
+               colour = FP_NADM_W_UNT,
+               size = FP_NADM_W_UNT,
+               label = CountryName)) +
+  geom_label(nudge_y = 0.3, size = 3,
+             family = 'Segoe UI Semilight') + 
+  geom_point() +
+  xlab('percent of women desiring more children') +
+  ggtitle('')+
+  ylab('total fertility rate')+
+  scale_size(guide = 'none') +
+  scale_colour_gradientn(colours = brewer.pal(9, 'RdPu'),
+                         name = 'unmet need',
+                         limits = c(7, 35)) +
+  theme_xygrid() +
+  guides(colour = guide_colorbar(ticks = FALSE))+ 
+  theme(legend.position = c(0.1, 0.9))
+
+ggsave("~/GitHub/tfr_babies_unmet.pdf",
+       width = 6, height = 4,
+       bg = 'transparent',
+       paper = 'special',
+       units = 'in',
+       useDingbats=FALSE,
+       compress = FALSE,
+       dpi = 300)
+
+# tfr unmet -----------------------------------------------------------
+
+
+ggplot(df, aes(x = 100- FP_NADM_W_UNT, y = tfr,
+               colour = FP_NADM_W_UNT,
+               size = FP_NADM_W_UNT,
+               label = CountryName)) +
+  geom_label(nudge_y = 0.3, size = 3,
+             family = 'Segoe UI Semilight') + 
+  geom_point() +
+  xlab('unmet neet') +
+  ggtitle('')+
+  ylab('total fertility rate')+
+  scale_size(guide = 'none') +
+  scale_colour_gradientn(colours = brewer.pal(9, 'RdPu'),
+                         name = 'unmet need',
+                         limits = c(7, 35)) +
+  theme_xygrid() +
+  guides(colour = guide_colorbar(ticks = FALSE))+ 
+  theme(legend.position = 'none')
+
+ggsave("~/GitHub/tfr_unmet.pdf",
+       width = 6, height = 4,
+       bg = 'transparent',
+       paper = 'special',
+       units = 'in',
+       useDingbats=FALSE,
+       compress = FALSE,
+       dpi = 300)
+
