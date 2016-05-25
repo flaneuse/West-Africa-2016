56cbf0fb40b490175ce2353cc9ae2c0c5353a105
flaneuse <laura.d.hughes@gmail.com>
2016-05-25 15:13:00 -0400

    unlist data

--- 2016-05-25_childMortCauses.R
+++ 2016-05-25_childMortCauses.R
@@ -3,15 +3,44 @@ loadPkgs()
 
 
 # Specify colors ----------------------------------------------------------
-
-
+colorMort = data.frame(cause = c(
+  "Acute lower respiratory infections",                      
+  "Birth asphyxia and birth trauma",                         
+  "Congenital anomalies",                                    
+  "Diarrhoeal diseases",                                     
+  "HIV/AIDS",                                                
+  "Injuries",                                                
+  "Malaria",                                                 
+  "Measles",                                                 
+  "Meningitis/encephalitis",                                 
+  "Other communicable, perinatal and nutritional conditions",
+  "Other noncommunicable diseases",                          
+  "Pertussis",                                               
+  "Prematurity",                                             
+  "Sepsis and other infectious conditions of the newborn",   
+  "Tetanus"),
+  color = c(
+    "#a6cee3",
+    "#1f78b4",
+    "#b2df8a",
+    "#33a02c",
+    "#fb9a99",
+    "#e31a1c",
+    "#fdbf6f",
+    "#ff7f00",
+    "#cab2d6",
+    "#6a3d9a",
+    "#ffff99",
+    "#b15928",
+    "")
+)
 
 # import child mortality cause data ---------------------------------------
 # Source: WHO Global Health Observatory
 
 # Find the names of all the data files (to merge together)
 allFiles = list.files('~/GitHub/West Africa 2016/ChildMortalityCauses/',
-           full.names = TRUE)
+                      full.names = TRUE)
 
 # Create a function to loop over all the data.
 
@@ -37,3 +66,16 @@ readGHOdata = function(fileName){
 }
 
 df = lapply(allFiles, readGHOdata)
+
+df2 = do.call(rbind, df)
+
+ggplot(df, aes(x = cause, 
+               y = mort,
+               fill = cause)) +
+  geom_bar(stat = 'identity') +
+  scale_fill_brewer(palette = 'Paired') +
+  # scale_fill_manual(values = colorMort$colors, 
+                    # breaks = colorMort$cause) +
+  theme_ygrid()
+
+ggsave()
