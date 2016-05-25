a9490c1fe95958de69da8973d637a29550af42fe
flaneuse <laura.d.hughes@gmail.com>
2016-05-25 17:00:19 -0400

    change proportion

--- 2016-05-25_childMortCauses.R
+++ 2016-05-25_childMortCauses.R
@@ -26,7 +26,7 @@ colorMort = data.frame(cause = c(causes, 'other'),
 # Source: WHO Global Health Observatory
 
 # Find the names of all the data files (to merge together)
-allFiles = list.files('~/GitHub/West Africa 2016/ChildMortalityCauses/',
+allFiles = list.files('~/GitHub/West Africa 2016/ChildDeathCauses_Proportion/',
                       full.names = TRUE)
 
 # Create a function to loop over all the data.
@@ -41,14 +41,15 @@ readGHOdata = function(fileName){
                           header = TRUE, skip = 0, nrows = 1)
   
   # Pull out the country name
-  countryName = as.character(countryName$Deaths.per.1.000.live.births)
+  countryName = as.character(countryName$Distribution.of.causes.of.death.among.children.aged...5.years....)
   
   # Clean up the data (throwing away data for children under 5, any time data)
   df = df %>%
     select(cause = Cause.of.death, 
            year = Year,
            mort = X0.4.years) %>% # Selecting just the data for the 
-    mutate(country = countryName) %>% 
+    mutate(country = countryName,
+           mort = mort / 100) %>% # Convert into ratio
     filter(year == max(year)) # Select the maximum year
 }
 
@@ -62,21 +63,21 @@ df = bind_rows(df)
 # reorder the graphs ------------------------------------------------------
 # simplify the causes of death
 df = df %>% 
-  mutate(causeCat = ifelse(cause %in% causes, as.character(cause), 'other'))
+  mutate(causeCat = ifelse(cause %in% causes, as.character(cause), 'other')) %>% 
+  filter(causeCat != 'other')
 
 # country
 df$country = factor(df$country, levels = countryOrder)
 
 # cause of death
 causeOrder = df %>% 
-  filter(causeCat != 'other') %>% 
   group_by(causeCat) %>% 
   summarise(total = sum(mort)) %>% 
   arrange(desc(total))
 
 
 # Reorder levels, putting other at the end.
-df$causeCat = factor(df$causeCat, levels = c(causeOrder$causeCat, 'other'))
+df$causeCat = factor(df$causeCat, levels = c(causeOrder$causeCat))
 
 # bar graph ---------------------------------------------------------------
 
@@ -85,18 +86,23 @@ ggplot(df, aes(x = causeCat,
                fill = causeCat,
                label = country)) +
   geom_bar(stat = 'identity') +
-  # scale_fill_brewer(palette = 'Set1') +
-  # scale_fill_manual(values = colorMort$colors,
-                    # breaks = colorMort$cause) +
+  scale_fill_brewer(palette = 'Paired') +
+  scale_y_continuous(labels = scales::percent,
+                     breaks = c(0, 0.25),
+                     minor_breaks = seq(0, 0.25, by = 0.125)) +
+  # scale_fill_manual(values = colorMort$color) +
   theme_ygrid() +
   facet_wrap(~ country, ncol = 1) +
-  geom_text(aes(x = 6, y = 20), hjust = 0, 
+  geom_text(aes(x = 6, y = 0.2), hjust = 0, 
             colour = grey75K,
             family = 'Segoe UI Light') +
   theme(strip.text = element_blank(),
         panel.background = element_rect(fill = NA, colour = grey75K, size = 0.1),
         axis.text.y = element_text(size = 6),
         axis.text.x = element_text(size = 8),
-        axis.title.y = element_blank())
+        axis.title.y = element_blank(),
+        legend.position = 'bottom',
+        legend.direction = 'horizontal',
+        panel.grid.minor.y = element_line(colour = grey60K, size = 0.1))
 
 # ggsave()
